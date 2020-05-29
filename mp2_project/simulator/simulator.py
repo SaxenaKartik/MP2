1de4aaf68b24b2df2012ad5304a0edafb06131d2
SaxenaKartik <kartiksaxena500@gmail.com>
2020-05-30 00:52:47 +0530

    Updated the simulator and test data

--- mp2_project/simulator/simulator.py
+++ mp2_project/simulator/simulator.py
@@ -4,6 +4,7 @@ import requests
 import hashlib
 from datetime import datetime
 import json
+import time
 
 def hashit(k : str):
     hash = hashlib.sha1()
@@ -17,11 +18,16 @@ class State:
         self.name = "Initial"
 
 class Information:
-    def __init__(self, hash_drone_id, battery_level):
-        self.drone_id = hash_drone_id
-        self.battery_level = battery_level
-        self.command = None
-
+    def __init__(self, entry):
+        self.drone_id = entry["drone_id"]
+        self.lat = entry["lat"]
+        self.log = entry["log"]
+        self.battery_level = entry["battery_level"]
+        self.users_connected = entry["users_connected"]
+        self.state = entry["state"]
+        self.warning_bit = True if entry["warning_bit"] else False
+        self.command = entry["command"]
+        self.state = entry["state"]
 
 
 class Simulator:
@@ -29,13 +35,26 @@ class Simulator:
         self.information = information
         self.state = State()
         self.observer = LocalServerObserver()
-
+        self.url = url
         # make post request to initiate the drone
-        response = requests.post(url, data = {'drone_id' : information.drone_id, 'battery_level' : information.battery_level})
-        if response.status_code==201:
-            self.observer.update("Added new drone to database")
+        entry = {
+            "drone_id" : self.information.drone_id,
+            "lat" : self.information.lat,
+            "log" : self.information.log,
+            "battery_level" : self.information.battery_level,
+            "users_connected" : self.information.users_connected,
+            "state" : self.information.state,
+            "warning_bit" : self.information.warning_bit,
+        }
+        connection = requests.get(self.url + str(information.drone_id))
+        if connection.status_code==200:
+            self.observer.update("Drone exists in db")
         else:
-            self.observer.update("Could not add drone to database")
+            response = requests.post(url, data = entry)
+            if response.status_code==201:
+                self.observer.update("Added new drone to database")
+            else:
+                self.observer.update("Could not add drone to database")
 
     def start(self, information):
         assert self.state.number == 0, "start() must be called after initial state"
@@ -44,7 +63,20 @@ class Simulator:
         self.information = information
 
         # make a post request based on information
-
+        entry = {
+            "drone_id" : self.information.drone_id,
+            "lat" : self.information.lat,
+            "log" : self.information.log,
+            "battery_level" : self.information.battery_level,
+            "users_connected" : self.information.users_connected,
+            "state" : self.information.state,
+            "warning_bit" : self.information.warning_bit,
+        }
+        response = requests.post(self.url, data = entry)
+        if response.status_code==201:
+            self.observer.update("Updated drone in database to state 1")
+        else:
+            self.observer.update("Could not update drone to database")
 
 
     def fly(self, information):
@@ -82,17 +114,15 @@ class Simulator:
 
         # make a post request based on information
 
-
-
 class Controller:
     def __init__(self, information, url):
         self.simulator = Simulator(information, url)
-        self.observer = Observer()
+        self.observer = LocalServerObserver()
 
     def start(self, information):
         state = {"Number" : self.simulator.state.number, "Name" : self.simulator.state.name}
         self.observer.update(state)
-        self.observer.update(information)
+        self.observer.update(information.command)
         self.simulator.start(information)
         state = {"Number" : self.simulator.state.number, "Name" : self.simulator.state.name}
         self.observer.update(state)
@@ -143,20 +173,47 @@ class View:
 
 class LocalServerView(View):
 
-    def __init__(self, hash_drone_id):
+    def __init__(self, information):
         # check if drone is already present in the database
         self.observer = LocalServerObserver()
-        self.information = Information(hash_drone_id, 100)
+        self.information = information
         self.url = "http://192.168.1.101:8080/api/drone/"
-        connection = requests.get(self.url + str(hash_drone_id))
-        if connection.status_code!=200:
-            self.controller = Controller(self.information, self.url)
-        else:
-            self.observer.update("Drone already exists in database")
+        # connection = requests.get(self.url + str(information.drone_id))
+        # if connection.status_code!=200:
+        self.controller = Controller(self.information, self.url)
+        # else:
+        # self.observer.update("Drone exists in database")
 
-    def fetch_information(self, hash_drone_id):
+    def fetch_information(self):
         # open a file to get local information
         # overwrite the server information based on local information
+        data = open("test_data.txt", "r")
+        data = json.loads(data.read())[-1]
+        # print(data["state"])
+        self.information = Information(data)
+
+        # print(self.information.state, self.controller.simulator.state.number)
+
+        if self.information.state == 1 and self.controller.simulator.state.number == 0:
+            # print("Here")
+            self.information.command = "start"
+
+        elif self.information.state == 2 and self.controller.simulator.state.number == 1:
+            self.information.command = "fly"
+
+        elif self.information.state == 3 and (self.controller.simulator.state.number == 1 or self.controller.simulator.state.number == 2):
+            self.information.command = "pause"
+
+        elif self.information.state == 4 and self.controller.simulator.state.number == 3:
+            self.information.command = "resume"
+
+        elif self.information.state == 5 and self.controller.simulator.state.number == 4:
+            self.information.command = "stop"
+            self.exit_code = 1
+
+        else:
+            self.information.command = ""
+        # print(self.information.command)
         return self.information
 
 
@@ -177,19 +234,43 @@ class LocalServerView(View):
         if information.command == "stop":
             self.controller.stop(information)
 
+        if information.command == "":
+            self.observer.update("No changes found! Maintaining the same state : " + str(self.controller.simulator.state.number))
 
 class Machine:
     def __init__(self):
-        self.drone_id = "00000000e215b4a2"
-        self.drone_id = hashit(self.drone_id)
-        print(self.drone_id)
-        self.view = LocalServerView(self.drone_id)
-        self.information = Information(self.drone_id, 100)
+        data = open("test_data.txt", "r")
+        entry = json.loads(data.read())[0]
+        # print(entry)
+        self.observer = LocalServerObserver()
+        # validating the entry
+
+        if "drone_id" not in entry or entry["drone_id"]=="":
+            self.observer.update("Invalid first entry! Must have drone_id for registration")
+            return
+
+        if entry["state"]!=0:
+            self.observer.update("Initial state must be 0")
+            return
+
+        if entry["warning_bit"]:
+            self.observer.update("Please check the battery, warning_bit has been set")
+
+        if entry["battery_level"]<30:
+            self.observer.update("Drone must be charged")
+            entry["warning_bit"] = True
+
+
+        entry["drone_id"] = hashit(entry["drone_id"])
+        # print(entry["drone_id"])
+        self.information = Information(entry)
+        data.close()
+        self.view = LocalServerView(self.information)
 
     def fetch(self):
-        information_set = self.view.fetch_information(self.drone_id)
+        information_set = self.view.fetch_information()
         self.information = information_set
-
+        # print(information_set.command)
     def send(self):
         self.view.send_information(self.information)
 
@@ -197,3 +278,11 @@ class Machine:
 obj = Machine()
 # obj.fetch()
 # obj.send()
+
+while(True):
+    obj.fetch()
+    time.sleep(1)
+    obj.send()
+    time.sleep(5)
+    if obj.exit_code==1:
+        obj.observer.update("Stopping the server")
